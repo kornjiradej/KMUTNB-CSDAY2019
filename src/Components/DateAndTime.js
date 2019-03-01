fa9b711336e288f80f7208805d4960f1c80f8dbc
Korn Jiradej <yommabancity@gmail.com>
2019-03-01 21:03:50 +0700

    Update Date&time

--- src/Components/DateAndTime.js
+++ src/Components/DateAndTime.js
@@ -1,126 +1,99 @@
 import React, { Component } from 'react';
 import { Element } from 'react-scroll'
-import { Row, Col } from 'antd';
 import '../Styles/DateAndTime.css';
 
 export default class DateAndTime extends Component {
+  state = {
+    activities: [
+      {
+        time: "วันที่ 7 มีนาคม 2018",
+        activity: ""
+      },{
+        time: "10.00 – 11.30 น.",
+        activity: "ลงทะเบียนผู้เข้าร่วมงาน ณ โถงชั้น 1 โซน A คณะวิทยาศาสตร์ประยุกต์"
+      }, {
+        time: "11.30 – 11.40 น.",
+        activity: "พิธีเปิด/กล่าวต้อนรับนักศึกษา/กล่าวต้อนรับบริษัทที่เข้ามาร่วมชมงาน"
+      }, {
+        time: "11.40 – 12.00 น.",
+        activity: "การแสดงโครงงานพิเศษและสหกิจศึกษาจากนักศึกษาภาควิชาฯ"
+      }, {
+        time: "12.00 – 13.00 น.",
+        activity: "รับประทานอาหารกลางวัน"
+      }, {
+        time: "13.00 – 16.00 น.",
+        activity: "การแสดงโครงงานพิเศษและสหกิจศึกษาจากนักศึกษาภาควิชาฯ"
+      }
+      , {
+        time: "วันที่ 8 มีนาคม 2562",
+        activity: ""
+      }
+      , {
+        time: "10.00 – 11.30 น.",
+        activity: "ลงทะเบียนผู้เข้าร่วมงาน ณ โถงชั้น 1 โซน A คณะวิทยาศาสตร์ประยุกต์"
+      }, {
+        time: "11.30 – 12.00 น.",
+        activity: "การแสดงโครงงานพิเศษและสหกิจศึกษาจากนักศึกษาภาควิชาฯ"
+      }, {
+        time: "12.00 – 13.00 น.",
+        activity: "รับประทานอาหารกลางวัน"
+      }, {
+        time: "13.00 – 16.00 น.",
+        activity: "การแสดงโครงงานพิเศษและสหกิจศึกษาจากนักศึกษาภาควิชาฯ"
+      }, {
+        time: "วันที่ 9 มีนาคม 2562",
+        activity: ""
+      }, {
+        time: "08.30 – 09.00 น.",
+        activity: "ลงทะเบียนผู้เข้าร่วมงาน ณ โถงชั้น 1 โซน A คณะวิทยาศาสตร์ประยุกต์"
+      }, {
+        time: "09.00 – 10.00 น.",
+        activity: "การแสดงโครงงานพิเศษและสหกิจศึกษาจากนักศึกษาภาควิชาฯ"
+      }, {
+        time: "10.00 – 11.00 น.",
+        activity: "Block Chain ในวงการอุตสาหกรรม ปัจจุบันโดย คุณพุทธิพร หงษ์สุรกุล บริษัทเอ็นเทอร์ คอร์ปอเรชั่น จำกัด "
+      }, {
+        time: "11.00 – 12.00 น.",
+        activity: "การนำ Image Processing ไปใช้ในวงการอุตสาหกรรมยุคปัจจุบัน โดย คุณประวีร์ เครือโชติกุล บริษัท โซลิแมค ออโตเมชั่น จำกัด "
+      }, {
+        time: "12.00 – 13.00 น.",
+        activity: "รับประทานอาหารกลางวัน"
+      }, {
+        time: "13.00 – 14.00 น.",
+        activity: "เทคโนโลยีที่ชาว CS ควรรู้ก่อนออกไปสู่ตลาดแรงงาน โดย คุณชาญ อริยกุล บริษัท กูเกิล คลาวด์ ไทยแลนด์"
+      }, {
+        time: "14.00 – 16.10 น.",
+        activity: "ประกาศผลรางวัลโครงงานยอดเยี่ยมของนักศึกษา/มอบรางวัล ผู้ได้รับรางวัล ขึ้นกล่าวบรรยายสรุปโครงงาน"
+      }, {
+        time: "16.10 – 16.20 น.",
+        activity: "พิธีปิด"
+      }
+    ]
+  }
   render() {
+    const {activities} = this.state;
+    const activityItems = activities.map(({activity, time}) => (
+      <div className="time-table-item" key={time}>
+        <div className="time">
+          <h4 data-aos="fade-right" style={{
+            margin: 0
+          }}>{time}</h4>
+        </div>
+        <div className="activity" data-aos="fade">
+          <h5 data-aos="fade-left" style={{
+            margin: 0
+          }}>{activity}</h5>
+        </div>
+      </div>
+    ))
     return (
-      <Element className="mgt-global-20" name="scroll-date">
-        <div className="DateAndTime" data-aos="fade-up">
-          <div className="left-side">
-            <h1 className="DateAndTimefont">DATE & TIME</h1>
-            <h2 className="fontMobileDate-DateHead" data-aos="fade-right">7 มีนาคม 2018</h2>
-            <h3 className="fontMobileDate" data-aos="fade-right">10.00 – 11.30 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">11.30 – 11.40 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">11.40 – 12.00 น</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">12.00 – 13.00 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">13.00 – 16.00 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate-DateHead" data-aos="fade-right">8 มีนาคม 2018</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">10.00 – 11.30 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">11.30 – 12.00 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">12.00 – 13.00 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">13.00 – 16.00 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-
-            <h2 className="fontMobileDate-DateHead" data-aos="fade-right">9 มีนาคม 2018</h2>
-            <h3 className="fontMobileDate" data-aos="fade-right">08.30 – 09.00 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">09.00 – 10.00 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">10.00 – 11.00 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">11.00 – 12.00 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">12.00 – 13.00 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">13.00 – 14.00 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">14.00 – 16.10 น.</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDate" data-aos="fade-right">16.10 – 16.20 น.</h3>
-
-            {/* <h2 data-aos="fade-right"> ** กำหนดการอาจมีการเปลี่ยนแปลงตามความเหมาะสม</h2> */}
-
-
+      <Element className="mgt-global-20" name="scroll-time">
+        <div className="Activities" data-aos="fade-up">
+          <div className="title-section">
+            <h1>DATE-TIME & DESCRIPTION</h1>
           </div>
-          <div className="right-side">
-            {/* งานวันที่ 7 */}
-            <h1 className="fontMobileDes-headDes" data-aos="fade-up">DESCRIPTION</h1>
-            <h2 className="fontMobileDes" data-aos="fade-left">&nbsp;</h2>
-            <h3 className="fontMobileDes" data-aos="fade-left">ลงทะเบียนผู้เข้าร่วมงาน ณ โถง </h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">ชั้น 1 โซน A คณะ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">วิทยาศาสตร์ประยุกต์</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">พิธีเปิด/กล่าวต้อนรับนักศึกษา/</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">กล่าวต้อนรับบริษัทที่เข้ามาร่วมชม</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">งาน</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">การแสดงโครงงานพิเศษและสหกิจ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">ศึกษาจากนักศึกษาภาควิชาฯ (78)</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">รับประทานอาหารกลางวัน</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">การแสดงโครงงานพิเศษและสหกิจ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">ศึกษาจากนักศึกษาภาควิชาฯ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">&nbsp;</h3>
-
-            {/* งานวันที่8 */}
-            <h3 className="fontMobileDes" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">ลงทะเบียนผู้เข้าร่วมงาน ณ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">โถงชั้น1 โซน A คณะ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">วิทยาศาสตร์ประยุกต์</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">การแสดงโครงงานพิเศษและสหกิจ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">ศึกษาจากนักศึกษาภาควิชาฯ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">รับประทานอาหารกลางวัน</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">การแสดงโครงงานพิเศษและสหกิจ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">ศึกษาจากนักศึกษาภาควิชาฯ</h3>
-
-            {/* งานวันที่9 */}
-            <h2 className="fontMobileDes" data-aos="fade-left">&nbsp;</h2>
-            <h3 className="fontMobileDes" data-aos="fade-left">&nbsp;</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">ลงทะเบียนผู้เข้าร่วมงาน ณ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">โถงชั้น1 โซน A คณะ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">วิทยาศาสตร์ประยุกต์</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">การแสดงโครงงานพิเศษและ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">สหกิจศึกษาจากนักศึกษาภาควิชาฯ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">“Block Chain ในวงการ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">อุตสาหกรรมปัจจุบัน”โดย</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">คุณพุทธิพร หงษ์สุรกุล</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">บริษัทเอ็นเทอร์ คอร์ปอเรชั่นจำกัด</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">“การนำ Image Processing ไป</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">ใช้ในวงการอุตสาหกรรมยุค</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">ปัจจุบัน” โดย</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">คุณประวีร์ เครือโชติกุล บริษัท</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">โซลิแมค ออโตเมชั่น จำกัด</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">รับประทานอาหารกลางวัน</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">“เทคโนโลยีที่ชาว CS ควรรู้ก่อน</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">ออกไปสู่ตลาดแรงงาน”</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">โดย คุณชาญ อริยกุล บริษัท</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">กูเกิล คลาวด์ ไทยแลนด์</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">ประกาศผลรางวัลโครงงาน</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">ยอดเยี่ยมของนักศึกษา/มอบ</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">รางวัลผู้ได้รับรางวัลขึ้น</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">กล่าวบรรยายสรุปโครงงาน</h3>
-            <h3 className="fontMobileDes" data-aos="fade-left">พิธีปิด</h3>
-
+          <div className="time-table">
+            {activityItems}
           </div>
         </div>
       </Element>
