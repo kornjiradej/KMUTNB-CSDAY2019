f98c8404c20d7e85b401476b1d50aa3342e46cfb
Korn Jiradej <yommabancity@gmail.com>
2019-03-02 16:59:51 +0700

    Update Date&time

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,7 +7,7 @@ import Detail from './Detail.js'
 import Location from './Location.js'
 import Contact from './Contact.js'
 import Footer from './Footer.js'
-// import Activities from './Activities';
+import Activities from './Activities';
 import AOS from 'aos';
 import {Element} from 'react-scroll'
 import StarfieldCanvas from './StarfieldCanvas';
